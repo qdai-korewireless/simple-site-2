6073763544748e182963c7c80330b58535902b09
Quan Dai <qdai@koresystemsgroup.com>
2017-07-15 17:23:53 -0500

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+var element = React.createElement('h1',{className:'greeting'},'Hello World!');
+ReactDOM.render(element, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
